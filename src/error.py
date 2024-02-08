8aeb0480d6f1f3f3b9b3db0321630407666b7930
surajkareppagol <surajkareppagol.dev@gmail.com>
2024-02-08 11:38:50 +0530

    🌱 Refactor code, remove methods

--- src/error.py
+++ src/error.py
@@ -5,41 +5,11 @@ class Error:
         Usage: error = Error()
         Return: None
         """
-        pass
 
-    def help(self) -> str:
-        """
-        Return help error string.
-        Usage: error.help()
-        Return: str
-        """
+        self.file_error = "[red bold]Error[/red bold]: Provide path value."
 
-        return """
-[bold green]CreateTT[/bold green]
-[bold red]Usage[/bold red]: [white]python3 main.py [...args][/white]
+        self.keyboard_interrupt_error = (
+            "\n\n[red bold]Error[/red bold]: Pressed [bold red]CTRL + C[/bold red]."
+        )
 
-[bold red]Args[/bold red]:
-    [bold yellow]-h, --help[/bold yellow]     Show help
-    [bold yellow]-a, --action[/bold yellow]   Action file
-    [bold yellow]-t, --time[/bold yellow]     Time file
-    [bold yellow]-d[/bold yellow]             Number of days
-    [bold yellow]-s, --save[/bold yellow]     Save, (html, svg, txt)
-        """
-
-    def file(self) -> str:
-        """
-        Return file error string.
-        Usage: error.file()
-        Return: str
-        """
-
-        return "[red bold]Error[/red bold]: Provide path value."
-
-    def keyboard_interrupt(self) -> str:
-        """
-        Return file error string.
-        Usage: error.file()
-        Return: str
-        """
-
-        return "\n\n[red bold]Error[/red bold]: Pressed [bold red]CTRL + C[/bold red]."
+        self.file_not_found_error = "[red bold]Error[/red bold]: File Not Found."
